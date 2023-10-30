448b44bec36063bf61edfa7453ecf25601a57521
Dylan Johnson <spfyyy@gmail.com>
2023-10-29 21:24:32 -0700

    rename ssh_config.example.c

---
+++ src/ssh_config.example.c
@@ -0,0 +1,8 @@
+#include "types.h"
+
+char *ssh_host = "localhost";
+u32 ssh_port = 22;
+char *ssh_user = "me";
+char *ssh_public_key_file = "/home/user/.ssh/key.pub"; // unix/linux
+char *ssh_private_key_file = "C:\\Users\\user\\.ssh\\key.priv"; // windows
+char *ssh_private_key_password = NULL;
